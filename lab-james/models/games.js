b79e80fe94be6285d70de0537e98b06942756016
jrzollin <jrzollin@yahoo.com>
2017-11-08 19:21:00 -0800

    found populate bug

--- lab-james/models/games.js
+++ lab-james/models/games.js
@@ -12,16 +12,16 @@ const gameSchema = mongoose.Schema({
 });
 
 gameSchema.pre('save', function(done){
-  Platform.findById(this.platform)
-    .then(passedPlatform => {
-      if(!passedPlatform){
+  Platform.findById(this.platforms)
+    .then(platform => {
+      if(!platform){
 
         return Promise.reject();
 
       } else {
 
-        this.platform = passedPlatform._id;
-        this.gameList = passedPlatform.gameList._id;
+        this.platforms = platform._id;
+        this.gameList = platform.gameList._id;
         return Promise.resolve(this.gameList);
 
       }
