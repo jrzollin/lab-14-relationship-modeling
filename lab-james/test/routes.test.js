10a0cb8cb93d28a83b7e5edb35fb70705d65ff16
jrzollin <jrzollin@yahoo.com>
2017-11-08 23:17:03 -0800

    tests done, modified code for found bugs

--- lab-james/test/routes.test.js
+++ lab-james/test/routes.test.js
@@ -26,8 +26,22 @@ describe('Games and Platforms tests', function(){
   });
 
   let platformId;
+  let gameId;
 
-  describe('platform creation', function(){
+  describe('Invalid Route', function(){
+
+    test('should respond with a 404 for invalid routes', function(){
+      return superagent.get('http://localhost:3000/wrong')
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toBe(404);
+        });
+
+    });
+
+  });
+
+  describe('Platform Creation', function(){
 
     test('should successfully create a new platform', function(){
       return superagent.post('http://localhost:3000/platforms')
@@ -45,9 +59,48 @@ describe('Games and Platforms tests', function(){
         });
     });
 
+    test('should respond with a 400 if no name set', function(){
+      return superagent.post('http://localhost:3000/platforms')
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toBe(400);
+        });
+    });
+
   });
 
-  describe('game creation', function(){
+  describe('Platform request', function(){
+
+    test('should succesfully retrieve a platform by ID', function(){
+      return superagent.get(`http://localhost:3000/platforms/${platformId}`)
+        .then(res => {
+          let text = JSON.parse(res.text);
+          expect(res.status).toEqual(200);
+          expect(text.name).toEqual('Nintendo');
+          expect(text._id).toEqual(platformId);
+        });
+    });
+
+    test('should send back an array with all platforms when no ID provided', function(){
+      return superagent.get('http://localhost:3000/platforms')
+        .then(res => {
+          let text = JSON.parse(res.text);
+          expect(res.status).toEqual(200);
+          expect(Array.isArray([text])).toBe(true);
+        });
+    });
+
+    test('should respond with 404 for bad ID', function(){
+      return superagent.get('http://localhost:3000/platforms/5')
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toBe(404);
+        });
+    });
+
+  });
+
+  describe('Game Creation', function(){
 
     test('should successfully create a new game', function(){
       return superagent.post('http://localhost:3000/games')
@@ -58,12 +111,123 @@ describe('Games and Platforms tests', function(){
         })
         .then(res => {
           let text = JSON.parse(res.text);
-          console.log(text);
+          gameId = text._id;
           expect(res.status).toEqual(200);
           expect(text.name).toEqual('Mario');
           expect(text.platforms).toEqual(platformId);
           expect(text.gameList).not.toBe(null);
           expect(text._id).not.toBe(null);
+        });
+    });
+
+    test('should respond with a 400 if body incomplete', function(){
+      return superagent.post('http://localhost:3000/games')
+        .set('content-type', 'application/json')
+        .send({
+          name: 'Zelda',
+        })
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toBe(400);
+        });
+    });
+
+  });
+
+  describe('Game Request', function(){
+
+    test('should successfully retrieve a game by ID', function(){
+      return superagent.get(`http://localhost:3000/games/${gameId}`)
+        .then(res => {
+          let text = JSON.parse(res.text);
+          expect(res.status).toBe(200);
+          expect(text.name).toEqual('Mario');
+          expect(text._id).toEqual(gameId);
+        });
+    });
+
+    test('should send back an array with all games when no ID provided', function(){
+      return superagent.get('http://localhost:3000/games')
+        .then(res => {
+          let text = JSON.parse(res.text);
+          expect(res.status).toBe(200);
+          expect(Array.isArray([text])).toBe(true);
+        });
+    });
+
+    test('should respond with a 404 for bad ID', function(){
+      return superagent.get('http://localhost:3000/5')
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toBe(404);
+        });
+    });
+
+  });
+
+  describe('Game Delete', function(){
+
+    test('should respond with a 200 when deleting a game by ID', function(){
+      return superagent.delete(`http://localhost:3000/games/${gameId}`)
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    test('deleted game should be empty', function(){
+      return superagent.get(`http://localhost:3000/games/${gameId}`)
+        .then(res => {
+          expect(res.text).toBe('');
+        });
+    });
+
+    test('should respond with a 400 if no ID was sent', function(){
+      return superagent.delete('http://localhost:3000/games')
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toBe(400);
+        });
+    });
+
+    test('should respond with a 404 if bad ID sent', function(){
+      return superagent.delete('http://localhost:3000/games/5')
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toBe(404);
+        });
+    });
+
+  });
+
+  describe('Platform Delete', function(){
+
+    test('should respond with a 200 when deleting a platform by ID', function(){
+      return superagent.delete(`http://localhost:3000/platforms/${platformId}`)
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    test('deleted platform should be empty', function(){
+      return superagent.get(`http://localhost:3000/platforms/${platformId}`)
+        .then(res => {
+          expect(res.text).toBe('');
+        });
+    });
+
+    test('should respond with a 400 if no ID was sent', function(){
+      return superagent.delete('http://localhost:3000/platforms')
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toBe(400);
+        });
+    });
+
+    test('should respond with a 404 if bad ID sent', function(){
+      return superagent.delete('http://localhost:3000/platforms/5')
+        .then(Promise.reject)
+        .catch(res => {
+          expect(res.status).toBe(404);
         });
     });
 
